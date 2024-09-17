2b3e5018300a1a294a95fc51afef61cdea17f325
Joy S <stepjoyous@gmail.com>
2024-09-16 23:49:59 -0700

    Create basic html table with aria roles.

---
+++ components/global/A11y_Examples/SrPlainHTMLTables/PlainHtmlTables/BaseHtmlTableWAria.jsx
@@ -0,0 +1,34 @@
+export const BaseHtmlTableWAria = () => {
+  return (
+    <>
+      <h2>Base HTML Table with Aria Roles</h2>
+      <h3>Plain HTML table. No aria roles/attributes and no scopes applied.</h3>
+      <table role="table">
+        <thead role="rowgroup">
+          <tr role="row">
+            <th role="columnheader">Date</th>
+            <th role="columnheader">Event</th>
+            <th role="columnheader">Venue</th>
+          </tr>
+        </thead>
+        <tbody role="rowgroup">
+          <tr role="row">
+            <td role="cell">March 12</td>
+            <td role="cell">Metallica</td>
+            <td role="cell">Staples Center</td>
+          </tr>
+          <tr role="row">
+            <td role="cell">June 11</td>
+            <td role="cell">Martin Garrix</td>
+            <td role="cell">Bill Graham Theatre</td>
+          </tr>
+          <tr role="row">
+            <td role="cell">November 15</td>
+            <td role="cell">Celine Dion</td>
+            <td role="cell">Madison Square Garden</td>
+          </tr>
+        </tbody>
+      </table>
+    </>
+  );
+};

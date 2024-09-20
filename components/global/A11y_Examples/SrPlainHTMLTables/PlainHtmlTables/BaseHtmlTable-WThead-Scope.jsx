150c484218ede4a70829bb9835cc3482c90aed14
Joy S <stepjoyous@gmail.com>
2024-09-19 23:39:21 -0700

    Create BaseHtmlTableWTheadAndScope and integrate into ExamplesData.

---
+++ components/global/A11y_Examples/SrPlainHTMLTables/PlainHtmlTables/BaseHtmlTable-WThead-Scope.jsx
@@ -0,0 +1,34 @@
+export const BaseHtmlTableWTheadAndScope = () => {
+  return (
+    <>
+      <h2>Base HTML Table with Aria Roles</h2>
+      <h3>Plain HTML table. No aria roles/attributes and no scopes applied.</h3>
+      <table>
+        <thead>
+          <tr>
+            <th scope="col">Date</th>
+            <th scope="col">Event</th>
+            <th scope="col">Venue</th>
+          </tr>
+        </thead>
+        <tbody>
+          <tr>
+            <td>March 12</td>
+            <td>Metallica</td>
+            <td>Staples Center</td>
+          </tr>
+          <tr>
+            <td>June 11</td>
+            <td>Martin Garrix</td>
+            <td>Bill Graham Theatre</td>
+          </tr>
+          <tr>
+            <td>November 15</td>
+            <td>Celine Dion</td>
+            <td>Madison Square Garden</td>
+          </tr>
+        </tbody>
+      </table>
+    </>
+  );
+};
--- components/global/A11y_Examples/SrPlainHTMLTables/PlainHtmlTables/ExamplesData.js
+++ components/global/A11y_Examples/SrPlainHTMLTables/PlainHtmlTables/ExamplesData.js
@@ -1,9 +1,11 @@
 import { BaseHtmlTableWThead } from "./BaseHtmlTable-WThead";
 import { BaseHtmlTableWTheadAndAria } from "./BaseHtmlTable-WThead-Aria";
+import { BaseHtmlTableWTheadAndScope } from "./BaseHtmlTable-WThead-Scope";
 import { BaseHtmlTableWTheadAndAriaAndScopes } from "./BaseHtmlTable-WThead-Aria&Scope";
 
 export const ExamplesData = [
   BaseHtmlTableWThead,
   BaseHtmlTableWTheadAndAria,
+  BaseHtmlTableWTheadAndScope,
   BaseHtmlTableWTheadAndAriaAndScopes
 ];

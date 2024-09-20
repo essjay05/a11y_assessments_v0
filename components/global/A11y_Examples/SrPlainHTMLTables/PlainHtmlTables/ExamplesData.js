89c28d4b3f6189d2fdecff630321a51013c0b2a8
Joy S <stepjoyous@gmail.com>
2024-09-19 23:36:39 -0700

    Implement BaseHtmlTableWTheadAriaAndScopes and add to ExamplesData.

--- components/global/A11y_Examples/SrPlainHTMLTables/PlainHtmlTables/ExamplesData.js
+++ components/global/A11y_Examples/SrPlainHTMLTables/PlainHtmlTables/ExamplesData.js
@@ -1,7 +1,9 @@
 import { BaseHtmlTableWThead } from "./BaseHtmlTable-WThead";
 import { BaseHtmlTableWTheadAndAria } from "./BaseHtmlTable-WThead-Aria";
+import { BaseHtmlTableWTheadAndAriaAndScopes } from "./BaseHtmlTable-WThead-Aria&Scope";
 
 export const ExamplesData = [
   BaseHtmlTableWThead,
-  BaseHtmlTableWTheadAndAria
+  BaseHtmlTableWTheadAndAria,
+  BaseHtmlTableWTheadAndAriaAndScopes
 ];

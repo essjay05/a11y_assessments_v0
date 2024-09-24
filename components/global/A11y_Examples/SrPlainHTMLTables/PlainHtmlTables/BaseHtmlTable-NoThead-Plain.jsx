4ad6aa6397126407a7501f76a1a50d5c36151d3c
Joy S <stepjoyous@gmail.com>
2024-09-23 23:25:40 -0700

    Added BaseHtmlTable no thead, no scope, and no aria-roles.

---
+++ components/global/A11y_Examples/SrPlainHTMLTables/PlainHtmlTables/BaseHtmlTable-NoThead-Plain.jsx
@@ -0,0 +1,29 @@
+export const BaseHtmlTableNoTheadPlain = () => {
+  return (
+    <>
+      <h2>5*. Table no thead/tbody (NO aria-roles AND NO scopes)</h2>
+    <table>
+      <tr>
+        <th>Date</th>
+        <th>Event</th>
+        <th>Venue</th>
+      </tr>
+      <tr>
+        <td>24 March</td>
+        <td>The What</td>
+        <td>Main Hall</td>
+      </tr>
+      <tr>
+        <td>12 February</td>
+        <td>Waltz with Strauss</td>
+        <td>Main Hall</td>
+      </tr>
+      <tr>
+        <td>14 April</td>
+        <td>The Obelisks</td>
+        <td>West Wing</td>
+      </tr>
+    </table>
+    </>
+  )
+}

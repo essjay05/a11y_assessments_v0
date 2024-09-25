ac9a4eba2f3856bd682b93e898b4a9ded1e9f721
Joy S <stepjoyous@gmail.com>
2024-09-24 20:26:49 -0700

    Create HTML table with no thead, no scopes, but with aria-roles.

---
+++ components/global/A11y_Examples/SrPlainHTMLTables/PlainHtmlTables/NoTheadWRoles.jsx
@@ -0,0 +1,29 @@
+export const NoTheadWRoles = () => {
+  return (
+    <>
+      <h2>5*. Table no thead/tbody (with aria-roles AND NO scopes)</h2>
+      <table>
+        <tr>
+          <th>Date</th>
+          <th>Event</th>
+          <th>Venue</th>
+        </tr>
+        <tr>
+          <td>24 March</td>
+          <td>The What</td>
+          <td>Main Hall</td>
+        </tr>
+        <tr>
+          <td>12 February</td>
+          <td>Waltz with Strauss</td>
+          <td>Main Hall</td>
+        </tr>
+        <tr>
+          <td>14 April</td>
+          <td>The Obelisks</td>
+          <td>West Wing</td>
+        </tr>
+      </table>
+    </>
+  )
+}

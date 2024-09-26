5e3defcf9b81a952f858db5f0db75cb7b2115076
Joy S <stepjoyous@gmail.com>
2024-09-25 22:46:12 -0700

    Added BaseHtmlTableNoTheadPlain to ExamplesData.

--- components/global/A11y_Examples/SrPlainHTMLTables/PlainHtmlTables/ExamplesData.js
+++ components/global/A11y_Examples/SrPlainHTMLTables/PlainHtmlTables/ExamplesData.js
@@ -1,11 +1,14 @@
+import { BaseHtmlTableNoTheadPlain } from "./BaseHtmlTable-NoTheadWRoles";
 import { BaseHtmlTableWThead } from "./BaseHtmlTable-WThead";
 import { BaseHtmlTableWTheadAndAria } from "./BaseHtmlTable-WThead-Aria";
 import { BaseHtmlTableWTheadAndScope } from "./BaseHtmlTable-WThead-Scope";
 import { BaseHtmlTableWTheadAndAriaAndScopes } from "./BaseHtmlTable-WThead-Aria&Scope";
+import { NoTheadWRoles } from "./NoTheadWRoles";
 
 export const ExamplesData = [
   BaseHtmlTableWThead,
   BaseHtmlTableWTheadAndAria,
   BaseHtmlTableWTheadAndScope,
-  BaseHtmlTableWTheadAndAriaAndScopes
+  BaseHtmlTableWTheadAndAriaAndScopes,
+  BaseHtmlTableNoTheadPlain
 ];

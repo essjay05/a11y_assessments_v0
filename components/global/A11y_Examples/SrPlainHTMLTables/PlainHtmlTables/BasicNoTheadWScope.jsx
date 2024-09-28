2ac2a95b23846e212ae43deaefe5964df69ca508
Joy S <stepjoyous@gmail.com>
2024-09-27 17:48:56 -0700

    Add BasicNoTheadWScope example.

---
+++ components/global/A11y_Examples/SrPlainHTMLTables/PlainHtmlTables/BasicNoTheadWScope.jsx
@@ -0,0 +1,29 @@
+export const BasicNoTheadWScope = () => {
+  return (
+    <>
+      <h2>6*. Table no thead/tbody (NO aria-roles AND WITH scopes)</h2>
+      <table id="BasicNoTheadWScope">
+        <tr>
+          <th scope="col">Date</th>
+          <th scope="col">Event</th>
+          <th scope="col">Venue</th>
+        </tr>
+        <tr>
+          <td>24 March</td>
+          <td>The What</td>
+          <td>Main Hall</td>
+        </tr>
+        <tr>
+          <td>12 February</td>
+          <td>Waltz with Strauss</td>
+          <td>West Wing</td>
+        </tr>
+        <tr>
+          <td>14 April</td>
+          <td>The Obelisks</td>
+          <td>Main Hall</td>
+        </tr>
+      </table>
+    </>
+  )
+}

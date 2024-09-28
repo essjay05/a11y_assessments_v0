74b91a9ec58f954c327b19a383ac78dc228bbbb2
Joy S <stepjoyous@gmail.com>
2024-09-27 17:51:41 -0700

    Added aria roles to BasicNoTheadWRoles.jsx

--- components/global/A11y_Examples/SrPlainHTMLTables/PlainHtmlTables/BasicNoTheadWRoles.jsx
+++ components/global/A11y_Examples/SrPlainHTMLTables/PlainHtmlTables/BasicNoTheadWRoles.jsx
@@ -2,26 +2,26 @@ export const BasicNoTheadWRoles = () => {
   return (
     <>
       <h2>5*. Table no thead/tbody (with aria-roles AND NO scopes)</h2>
-      <table id="BasicNoTheadWRoles">
-        <tr>
-          <th>Date</th>
-          <th>Event</th>
-          <th>Venue</th>
+      <table id="BasicNoTheadWRoles" role="table">
+        <tr role="row">
+          <th role="columnheader">Date</th>
+          <th role="columnheader">Event</th>
+          <th role="columnheader">Venue</th>
         </tr>
-        <tr>
-          <td>24 March</td>
-          <td>The What</td>
-          <td>Main Hall</td>
+        <tr role="row">
+          <td role="cell">24 March</td>
+          <td role="cell">The What</td>
+          <td role="cell">Main Hall</td>
         </tr>
-        <tr>
-          <td>12 February</td>
-          <td>Waltz with Strauss</td>
-          <td>Main Hall</td>
+        <tr role="row">
+          <td role="cell">12 February</td>
+          <td role="cell">Waltz with Strauss</td>
+          <td role="cell">Main Hall</td>
         </tr>
-        <tr>
-          <td>14 April</td>
-          <td>The Obelisks</td>
-          <td>West Wing</td>
+        <tr role="row">
+          <td role="cell">14 April</td>
+          <td role="cell">The Obelisks</td>
+          <td role="cell">West Wing</td>
         </tr>
       </table>
     </>

f626ec2c7b453dbaec862d142dc46cbbfe406753
Joy S <stepjoyous@gmail.com>
2024-10-01 16:33:26 -0700

    Fixed typo in title of BasicNotHeadWRolesWScope file.

--- components/global/A11y_Examples/SrPlainHTMLTables/PlainHtmlTables/BasicNoTheadWRolesWScope.jsx
+++ components/global/A11y_Examples/SrPlainHTMLTables/PlainHtmlTables/BasicNoTheadWRolesWScope.jsx
@@ -1,7 +1,7 @@
 export const BasicNoTheadWRolesWScope = () => {
   return (
     <>
-      <h2>Table no thead/tbody (with aria-roles AND NO scopes)</h2>
+      <h2>Table no thead/tbody (with aria-roles AND with scopes)</h2>
       <table id="BasicNoTheadWRolesWScope" role="table">
         <tr role="row">
           <th role="columnheader" scope="col">Date</th>
